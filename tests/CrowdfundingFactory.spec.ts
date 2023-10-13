0088e7081e623d11e20356957b00391dad0043c2
Tikhon Slasten <slasten32@gmail.com>
2023-10-13 09:53:12 +0800

    Add tests for CrowdfundingFactory

--- tests/CrowdfundingFactory.spec.ts
+++ tests/CrowdfundingFactory.spec.ts
@@ -1,18 +1,24 @@
-import { Blockchain, SandboxContract } from '@ton-community/sandbox';
+import { Blockchain, SandboxContract, TreasuryContract } from '@ton-community/sandbox';
 import { toNano } from 'ton-core';
 import { CrowdfundingFactory } from '../wrappers/CrowdfundingFactory';
+import { getUnixTimestampNow } from './utils';
 import '@ton-community/test-utils';
+
+const MIN_VALUE_TO_START = toNano('1');
+const MAX_DEADLINE = 365 * 24 * 60 * 60;
+const ONE_DAY: number = 1 * 24 * 60 * 60;
 
 describe('CrowdfundingFactory', () => {
     let blockchain: Blockchain;
     let crowdfundingFactory: SandboxContract<CrowdfundingFactory>;
+    let deployer: SandboxContract<TreasuryContract>;
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
 
         crowdfundingFactory = blockchain.openContract(await CrowdfundingFactory.fromInit());
 
-        const deployer = await blockchain.treasury('deployer');
+        deployer = await blockchain.treasury('deployer');
 
         const deployResult = await crowdfundingFactory.send(
             deployer.getSender(),
@@ -33,8 +39,73 @@ describe('CrowdfundingFactory', () => {
         });
     });
 
-    it('should deploy', async () => {
-        // the check is done inside beforeEach
-        // blockchain and crowdfundingFactory are ready to use
+    it('increase seqno after creating the crowdfunding', async () => {
+        let seqno: bigint = await crowdfundingFactory.getGetLastSeqno();
+        expect(seqno).toEqual(0n);
+
+        await startCrowdfunding();
+
+        seqno = await crowdfundingFactory.getGetLastSeqno();
+        expect(seqno).toEqual(1n);
     });
+
+    it("don't start crowdfunding if value is not enough", async () => {
+        await crowdfundingFactory.send(
+            deployer.getSender(),
+            {
+                value: MIN_VALUE_TO_START - toNano('0.01'),
+            },
+            {
+                $$type: 'CrowdfundingParams',
+                title: 'Test Title',
+                description: 'Test Description',
+                minContribution: toNano('0.01'),
+                targetContribution: toNano('5'),
+                deadline: BigInt(getUnixTimestampNow()),
+                beneficiary: deployer.getSender().address,
+            },
+        );
+
+        const seqno: bigint = await crowdfundingFactory.getGetLastSeqno();
+        expect(seqno).toEqual(0n);
+    });
+
+    it("don't start crowdfunding if deadline is too big", async () => {
+        await crowdfundingFactory.send(
+            deployer.getSender(),
+            {
+                value: MIN_VALUE_TO_START,
+            },
+            {
+                $$type: 'CrowdfundingParams',
+                title: 'Test Title',
+                description: 'Test Description',
+                minContribution: toNano('0.01'),
+                targetContribution: toNano('5'),
+                deadline: BigInt(getUnixTimestampNow() + MAX_DEADLINE + ONE_DAY),
+                beneficiary: deployer.getSender().address,
+            },
+        );
+
+        const seqno: bigint = await crowdfundingFactory.getGetLastSeqno();
+        expect(seqno).toEqual(0n);
+    });
+
+    const startCrowdfunding = async () => {
+        await crowdfundingFactory.send(
+            deployer.getSender(),
+            {
+                value: MIN_VALUE_TO_START,
+            },
+            {
+                $$type: 'CrowdfundingParams',
+                title: 'Test Title',
+                description: 'Test Description',
+                minContribution: toNano('0.01'),
+                targetContribution: toNano('5'),
+                deadline: BigInt(getUnixTimestampNow() + ONE_DAY),
+                beneficiary: deployer.getSender().address,
+            },
+        );
+    };
 });
